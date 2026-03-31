effc11d2a0cde053148ccf4bb6a2747aee29af06
svlandeg <svlandeg@github.com>
2026-03-31 15:12:13 +0200

    avoid eval entirely for sft

--- runs/runcpu.sh
+++ runs/runcpu.sh
@@ -34,7 +34,7 @@ python -m scripts.base_eval --device-batch-size=1 --split-tokens=16384 --max-per
 
 # SFT (~10 minutes on my MacBook Pro M3 Max)
 curl -L -o $NANOCHAT_BASE_DIR/identity_conversations.jsonl https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
-python -m scripts.chat_sft --max-seq-len=512 --device-batch-size=32 --total-batch-size=16384 --eval-every=-1 --eval-tokens=524288 --num-iterations=10
+python -m scripts.chat_sft --eval-every=-1 --num-iterations=10
 
 # Chat with the model over CLI
 # The model should be able to say that it is Paris.
