37ce94dfcdf15535d31166476f14bd5988e4c7d3
svlandeg <svlandeg@github.com>
2026-04-01 14:09:44 +0200

    reset

--- demo_runs/2_pretrain.sh
+++ demo_runs/2_pretrain.sh
@@ -4,8 +4,8 @@ source .venv/bin/activate
 
 # use previously trained base model
 # python -m scripts.base_train --depth=6 --device-batch-size=16 --window-pattern="L"
-# export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat_run1_base50"
-export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat_run2_base500"
+export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat_run1_base50"
+# export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat_run2_base500"
 
 echo "ll $NANOCHAT_BASE_DIR"
 ls -al $NANOCHAT_BASE_DIR
