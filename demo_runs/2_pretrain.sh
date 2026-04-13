23322b02f56ce106162a5e555789928fa3d88b54
svlandeg <svlandeg@github.com>
2026-04-13 08:24:44 +0000

    fix

--- demo_runs/2_pretrain.sh
+++ demo_runs/2_pretrain.sh
@@ -2,8 +2,8 @@
 
 # use previously trained base model
 # python -m scripts.base_train --depth=14 --device-batch-size=16 --window-pattern="L"
-export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat_run33_d6"
-# export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat_run22_d14"
+# export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat_run33_d6"
+export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat_run22_d14"
 
 echo "ll $NANOCHAT_BASE_DIR"
 ls -al $NANOCHAT_BASE_DIR
